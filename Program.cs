e036f97c69803430c4d909c5d84f68aceafffe18
Arun Kumar Cheemakurthi <akarunkumar3312@gmail.com>
2025-08-02 18:41:35 -0500

    commit --

--- Program.cs
+++ Program.cs
@@ -4,16 +4,21 @@ using Microsoft.EntityFrameworkCore.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Use connection string from appsettings.json
 builder.Services.AddDbContext<EquinoxContext>(options =>
-options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-// options.UseSqlite("Data Source=Equinox.db"));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddSession();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Automatically apply migrations and create database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<EquinoxContext>();
+    db.Database.Migrate();
+}
 
 app.UseStaticFiles();
 app.UseRouting();
@@ -30,4 +35,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
