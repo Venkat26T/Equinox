0dfc304f6f5b23420f078c2c33bc8e554daef47e
Fatheen <fatheenfarhan1234@gmail.com>
2025-08-03 15:50:24 -0500

    commit db

--- obj/Debug/net9.0/Equinox.AssemblyInfo.cs
+++ obj/Debug/net9.0/Equinox.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Equinox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3054c1377453d1e53eb4e745217a4a5f56d8dddb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b773ca6cc4f6cd83fc4235e7cb66139c8594e4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Equinox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Equinox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
